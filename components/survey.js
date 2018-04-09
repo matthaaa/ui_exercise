528330ae5b3bcdb4dcdabeca56b1d2213fda93b1
Matt Ha <matthewha1993@gmail.com>
2018-04-09 15:49:37 -0700

    Add booleans for state of question form

--- components/survey.js
+++ components/survey.js
@@ -1,6 +1,10 @@
 new Vue({
   el: '#survey',
   data: {
+    newQuestion: true,
+    newAnswers: false,
+    review: false,
+    complete: false,
     formQuestion: "",
     questions: {},
   },
@@ -8,8 +12,11 @@ new Vue({
     saveQuestion: function () {
       const questionNumber = Object.keys(this.questions).length + 1;
       this.questions[questionNumber] = this.formQuestion;
-      formQuestion = "";
+      this.formQuestion = "";
+      this.newQuestion = false;
+      this.newAnswers = true;
       console.log(this.questions);
+      console.log();
     }
   }
 })
